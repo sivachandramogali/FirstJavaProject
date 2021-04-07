4ea4a29d0b778802e328613d3f6ecefc7bc5f3cc
sivachandramogali <sivachandramogali2001@gmail.com>
2021-04-07 13:22:41 +0530

    greater then condition sting operation count success with command line aruguments

--- src/First.java
+++ src/First.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class First {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        int a1[]=new int[10];
+        int b1[]=new int[10];
 
         System.out.println("Enter String a");
-        String a =sc.next();
+        String a =args[0];
 
         System.out.println("Enter String b");
-        String b =sc.next();
-        System.out.println("Enter String c");
-        String c =sc.next();
+        String b =args[1];
+        /*System.out.println("Enter String c");
+        String c =sc.next();*/
 
         System.out.println("Sting concatination "+a+b);
         int count=0;
-        if(a.length()==b.length())
+        if(a.length()==b.length() )
         {
             for(int i=0;i<a.length();i++){
                 if(a.charAt(i) != b.charAt(i)){
@@ -25,7 +27,67 @@ public class First {
             }
         }
         System.out.println("Number of chareacter differnt in 2 stings \n"+count );
-        if(a.length()<=b.length()){
+        if(a.length()>=b.length()){
+            System.out.println("greater than condition");
+            for(int i=0;i<a.length();i++){
+                count=0;
+                for( int j=0;j<b.length();j++){
+                    if(a.charAt(i) == b.charAt(j)){
+                        count++;
+                    }
+                    a1[i]=count;
+                }
+            }
+
+            for(int k = 0; k <b.length(); k++){
+                count=0;
+                for(int j = 0; j<a.length(); j++){
+                    if(b.charAt(k) == a.charAt(j)){
+                        count++;
+                    }
+                    b1[k]=count;
+                }
+            }
+            System.out.println("counts assigned to respective array");
+
+            int n=0;
+            count=0;
+            for(int m = 0; m <b.length(); m++){
+                    if(b1[m]==0&&a1[n]==0){
+                        count++;
+                        System.out.println("Number operation performed for sting converter"+count);
+                        n++;
+
+                    }
+                    else if (b1[m]!=0&&a1[n]==0){
+                        n++;
+                        m--;
+                        count++;
+                        System.out.println("Number operation performed for sting converter"+count);
+
+                        /*if(a1[n]!=0&&b.charAt(m)==a.charAt(n)){
+                            count++;
+                            System.out.println("Number operation performed for sting converter"+count);
+                        }*/
+                    }
+                    else if(b1[m]!=0&&a1[n]!=0){
+                        System.out.println("not 0 not 0 condition...."+a.charAt(n)+"..... "+b.charAt(m));
+                        if(a.charAt(n)!=b.charAt(m))
+                        {
+                            m--;
+                            count++;
+                            n++;
+                            System.out.println("Number operation performed for sting converter"+count);
+                        }
+                        else if(a.charAt(n)==b.charAt(m)){
+                            n++;
+                            System.out.println("eqUAL CONDITION");
+                        }
+                    }
+            }
+            System.out.println("Number operation performed for sting converter"+count);
+
+
 
         }
     }
