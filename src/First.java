7591f0c7dbebb66ab23b86684c64fb6ea69cece4
sivachandramogali <sivachandramogali2001@gmail.com>
2021-04-13 10:21:29 +0530

    validate the testcases

--- src/First.java
+++ src/First.java
@@ -14,10 +14,12 @@ public class First {
         /*System.out.println("Enter String c");
         String c =sc.next();*/
 
-        System.out.println("Sting concatination "+a+b);
+        System.out.println("String concatination "+a+b);
         int count=0;
         if(a.length()==b.length() )
         {
+            System.out.println("2 Stings are equal");
+
             for(int i=0;i<a.length();i++){
                 if(a.charAt(i) != b.charAt(i)){
                     System.out.println(a.charAt(i));
@@ -25,9 +27,9 @@ public class First {
                 }
 
             }
+            System.out.println("Number of chareacter differnt in 2 stings \n"+count );
         }
-        System.out.println("Number of chareacter differnt in 2 stings \n"+count );
-        if(a.length()>=b.length()){
+        if(a.length()>b.length()){
             System.out.println("greater than condition");
             for(int i=0;i<a.length();i++){
                 count=0;
@@ -46,24 +48,37 @@ public class First {
                         count++;
                     }
                     b1[k]=count;
-                }
-            }
-            System.out.println("counts assigned to respective array");
 
-            int n=0;
+                }
+                System.out.println("counts assigned to respective array"+b1[k]);
+
+            }
+            System.out.println("counts assigned to respective array"+b1+" "+a1);
+
+            int n=0,m=0;
+
             count=0;
-            for(int m = 0; m <b.length(); m++){
-                    if(b1[m]==0&&a1[n]==0){
+            while (true){
+                System.out.println(m+" .........for loop m value\n");
+                    if(b1[m+1]=='\0'&&a1[m+1]=='\0')
+                    {
+                        break;
+                    }
+                   else if(b1[m]==0&&a1[n]==0){
                         count++;
                         System.out.println("Number operation performed for sting converter"+count);
                         n++;
+                        m++;
+                        System.out.println("n value ..."+n);
 
                     }
                     else if (b1[m]!=0&&a1[n]==0){
+
                         n++;
-                        m--;
                         count++;
                         System.out.println("Number operation performed for sting converter"+count);
+                        System.out.println("n value ..."+n);
+
 
                         /*if(a1[n]!=0&&b.charAt(m)==a.charAt(n)){
                             count++;
@@ -74,14 +89,26 @@ public class First {
                         System.out.println("not 0 not 0 condition...."+a.charAt(n)+"..... "+b.charAt(m));
                         if(a.charAt(n)!=b.charAt(m))
                         {
-                            m--;
+
                             count++;
                             n++;
                             System.out.println("Number operation performed for sting converter"+count);
+                            System.out.println("n value ..."+n);
+
                         }
                         else if(a.charAt(n)==b.charAt(m)){
                             n++;
+                            m++;
                             System.out.println("eqUAL CONDITION");
+                            System.out.println("n value ..."+n);
+
+                        }
+                    }
+                    else{
+                        if(a.charAt(n)!='\0'){
+                            count++;
+                            n++;
+                            System.out.println("n value ..."+n);
                         }
                     }
             }
