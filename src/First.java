9374256efe19846b3a98402fa812e2a14ed8dfb6
sivachandramogali <sivachandramogali2001@gmail.com>
2021-04-21 11:40:49 +0530

    sucessfully excuted

--- src/First.java
+++ src/First.java
@@ -1,5 +1,13 @@
 public class First {
     public static void main(String args[]){
         System.out.println("Jai Hind");
+        boolean  flag= true;
+        if (flag){
+            System.out.println("Flag is set to true");
+        }
+        else
+            System.out.println( "Flag is set to flase");
+        System.out.println("Printing Flag variable");
     }
+
 }
